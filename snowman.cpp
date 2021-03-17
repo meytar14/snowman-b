22f944eae9b054a0a369fab38dae684e763320e3
Meytar <meytar147@gmail.com>
2021-03-17 19:46:58 +0200

    second commit

--- snowman.cpp
+++ snowman.cpp
@@ -16,18 +16,30 @@ namespace ariel{
     array<string,4> rightArm ={">","/","\\"," "};
     array<string,4> torso={"( : )","(] [)","(> <)","(   )"};
     array<string,4> base={"( : )","(\" \")","(___)","(   )"};
+    const int ten=10;
+    const int hundred=100;
+    const int thousend=1000;
+    const int two=2;
 
-
-    string snowman(int x)
+    int checkRightHandUp(int x)
+    {
+        if((x/hundred)%ten==two)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    int checkLeftHandUp(int x)
+    {
+        if((x/(thousend))%ten==two)
+        {
+           return 1;
+        }
+        return 0;
+    }
+    void checkValidInput(int x)
     {
         int temp=x;
-        
-        const int ten=10;
-        const int hundred=100;
-        const int thousend=1000;
-        
-
-        //check if the input is valid
         const int eight=8;
         const int four=4;
         for(int i=0;i<eight;i++)
@@ -42,26 +54,15 @@ namespace ariel{
         {
             throw invalid_argument("invalid input");
         }
-
-
-        
-       
-        bool isRightArmUp=false;
-        bool isLeftArmUp=false;
-        if((x/hundred)%ten==2)
-        {
-            isRightArmUp=true;
-        }
-         if((x/(thousend))%ten==2)
-        {
-            isLeftArmUp=true;
-        }
-
-        
-        //build the snowman
+    }
+    string Hat(int x)
+    {
+       return hat.at((x/(ten*thousend*thousend))-1);
+    }
+    string head(int x,int isLeftArmUp,int isRightArmUp )
+    {
         string s;
-        s=s+hat.at((x/(ten*thousend*thousend))-1);
-        if(isLeftArmUp)
+        if(isLeftArmUp!=0)
         {
             s=s+leftArm.at(1);
         }
@@ -71,14 +72,19 @@ namespace ariel{
         s=s+"("+leftEye.at(((x/(hundred*thousend))%ten)-1);
         s=s+nose.at(((x/(thousend*thousend))%ten)-1);
         s=s+rightEye.at(((x/(ten*thousend))%ten)-1)+")";
-        if(isRightArmUp)
+        if(isRightArmUp!=0)
         {
             s=s+rightArm.at(1)+"\n";
         }
         else{
             s=s+"\n";
         }
-        if(!isLeftArmUp)
+        return s;
+    }
+    string body(int x,int isLeftArmUp,int isRightArmUp)
+    {
+        string s;
+        if(isLeftArmUp==0)
         {
             s=s+leftArm.at(((x/(thousend))%ten)-1);
         }
@@ -86,7 +92,7 @@ namespace ariel{
             s=s+" ";
         }
         s=s+torso.at(((x/ten)%ten)-1);
-        if(!isRightArmUp)
+        if(isRightArmUp==0)
         {
             s=s+rightArm.at(((x/hundred)%ten)-1)+"\n";
         }
@@ -94,7 +100,94 @@ namespace ariel{
         {
             s=s+"\n";
         }
-        s=s+" "+base.at((x%ten)-1);
+        return s;
+    }
+    string Base(int x)
+    {
+        string s=" "+base.at((x%ten)-1);
+        return s;
+    }
+
+
+    string snowman(int x)
+    {
+        
+        
+        const int ten=10;
+        const int hundred=100;
+        const int thousend=1000;
+        
+
+        //check if the input is valid
+        checkValidInput(x);
+        // const int eight=8;
+        // const int four=4;
+        // for(int i=0;i<eight;i++)
+        // {
+        //     if(temp%ten>four||temp%ten<1)
+        //     {
+        //         throw invalid_argument("invalid input");
+        //     }
+        //     temp=temp/ten;
+        // }
+        // if(temp!=0)
+        // {
+        //     throw invalid_argument("invalid input");
+        // }
+
+
+        
+       
+        int isRightArmUp=checkRightHandUp(x);
+        int isLeftArmUp=checkLeftHandUp(x);
+        // if((x/hundred)%ten==2)
+        // {
+        //     isRightArmUp=true;
+        // }
+        //  if((x/(thousend))%ten==2)
+        // {
+        //     isLeftArmUp=true;
+        // }
+
+
+        //build the snowman
+        string s;
+        s=Hat(x)+head(x, isLeftArmUp, isRightArmUp)+body(x, isLeftArmUp, isRightArmUp)+Base(x);
+        // s=s+hat.at((x/(ten*thousend*thousend))-1);
+        // if(isLeftArmUp)
+        // {
+        //     s=s+leftArm.at(1);
+        // }
+        // else{
+        //     s=s+" ";
+        // }
+        // s=s+"("+leftEye.at(((x/(hundred*thousend))%ten)-1);
+        // s=s+nose.at(((x/(thousend*thousend))%ten)-1);
+        // s=s+rightEye.at(((x/(ten*thousend))%ten)-1)+")";
+        // if(isRightArmUp)
+        // {
+        //     s=s+rightArm.at(1)+"\n";
+        // }
+        // else{
+        //     s=s+"\n";
+        // }
+        // if(!isLeftArmUp)
+        // {
+        //     s=s+leftArm.at(((x/(thousend))%ten)-1);
+        // }
+        // else{// add " "before the torso
+        //     s=s+" ";
+        // }
+        // s=s+torso.at(((x/ten)%ten)-1);
+        // if(!isRightArmUp)
+        // {
+        //     s=s+rightArm.at(((x/hundred)%ten)-1)+"\n";
+        // }
+        // else
+        // {
+        //     s=s+"\n";
+        // }
+        // s=s+" "+base.at((x%ten)-1);
         return s;
     }
 }
